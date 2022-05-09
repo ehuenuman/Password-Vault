95b24f6e481810b557b50b373d56b108029739a4
Esteban <esteban.huenuman@hotmail.cl>
2022-05-09 17:58:18 +1200

    First use of NativeBase to build the register screen

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NativeBaseProvider, Box } from 'native-base';
+
+import CreateAccount from './app/screens/CreateAccount';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Box
+        flex={1}
+        justifyContent='center'
+        alignItems='center'
+      >
+        <CreateAccount />
+      </Box>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
