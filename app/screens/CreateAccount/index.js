f93bab2501d2d662d6dfec18bdbdfa2fc4d67cc8
Esteban <esteban.huenuman@hotmail.cl>
2022-05-11 01:37:41 +1200

    New directory and name

---
+++ app/screens/CreateAccount/index.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Center,
+  FormControl,
+  Input,
+  Slider,
+  Text,
+  VStack,
+} from 'native-base';
+
+function CreateAccount() {
+
+  const [passwordConditions, setPasswordConditions] = useState([]);
+
+  return (
+    <Center
+      flex={1}
+    >
+      <Box
+        maxWidth={300}
+      >
+        <Box>
+          <FormControl isRequired isInvalid >
+            <FormControl.Label>NAME</FormControl.Label>
+            <Input p={2} />
+          </FormControl>
+          <FormControl isRequired isInvalid >
+            <FormControl.Label>EMAIL</FormControl.Label>
+            <Input p={2} />
+          </FormControl>
+          <FormControl isRequired isInvalid >
+            <FormControl.Label>MASTER PASSWORD</FormControl.Label>
+            <Input p={2} />
+          </FormControl>
+          <Box
+            m={2}
+          >
+            <Box
+              marginBottom="3"
+            >
+              <Slider
+                defaultValue={0}
+                maxValue={100}
+                step={1}
+                marginBottom="-2"
+              >
+                <Slider.Track >
+                  <Slider.FilledTrack />
+                </Slider.Track>
+              </Slider>
+              <Box
+                flexDirection="row"
+                justifyContent="space-between"
+                px={2}
+              >
+                <Text fontSize="xs">Low</Text>
+                <Text fontSize="xs">High</Text>
+              </Box>
+            </Box>
+            <Checkbox.Group
+              size="sm"
+              px={2}
+              defaultValue={passwordConditions}
+              onChange={values => {
+                setPasswordConditions(values || []);
+              }}
+            >
+              <Checkbox isDisabled value="Numbers" my="1">
+                Numbers
+              </Checkbox>
+              <Checkbox isDisabled value="Letters" my="1">
+                Letters
+              </Checkbox>
+              <Checkbox isDisabled value="Symbols" my="1">
+                Symbols
+              </Checkbox>
+            </Checkbox.Group>
+          </Box>
+          <FormControl isRequired isInvalid >
+            <FormControl.Label>CONFIRM PASSWORD</FormControl.Label>
+            <Input p={2} />
+          </FormControl>
+        </Box>
+        <VStack
+          space={2}
+          mt={16}
+        >
+          <Text textAlign="center">
+            Clicking on continue button you are accepting our terms and conditions.
+          </Text>
+          <Button
+            colorScheme="primary"
+            onPress={() => {
+              console.log('hello')
+            }}
+          >
+            CONTINUE
+          </Button>
+        </VStack>
+      </Box >
+    </Center>
+  );
+}
+
+export default CreateAccount;
