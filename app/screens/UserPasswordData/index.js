7e720395d73af2e79f3a71905acba67c7afbae72
Esteban <esteban.huenuman@hotmail.cl>
2022-05-11 20:41:44 +1200

    Added ScrollView

--- app/screens/UserPasswordData/index.js
+++ app/screens/UserPasswordData/index.js
@@ -3,37 +3,40 @@ import React, { useState } from 'react';
 import {
   Box,
   Image,
+  ScrollView,
 } from 'native-base';
-
 import AppBar from './components/AppBar';
+
 import Form from './components/Form';
 
 export default function UserPasswordData() {
 
-  const [formAction, setFormAction] = useState("view"); // "view"|"edit"|"new"
+  const [formAction, setFormAction] = useState("new"); // "view"|"edit"|"new"
 
   const changeFormAction = (action) => {
-    console.log(action);
     setFormAction(action);
   }
 
   return (
     <Box safeAreaTop>
       <AppBar action={formAction} actionChangeHandler={changeFormAction} />
-      <Box
-        px="15%"
-      >
-        <Image
-          source={{
-            uri: "https://wallpaperaccess.com/full/317501.jpg"
-          }}
-          alt="Brand Logo"
-          size="85px"
-          my={10}
-          alignSelf="center"
-        />
-        <Form viewMode={formAction == "view" ? true : false} />
-      </Box>
+
+      <ScrollView>
+        <Box
+          px="15%"
+        >
+          <Image
+            source={{
+              uri: "https://wallpaperaccess.com/full/317501.jpg"
+            }}
+            alt="Brand Logo"
+            size="85px"
+            my="10"
+            alignSelf="center"
+          />
+          <Form viewMode={formAction == "view" ? true : false} modeForm={formAction} actionChangeHandler={changeFormAction} />
+        </Box>
+      </ScrollView>
     </Box>
 
   )
