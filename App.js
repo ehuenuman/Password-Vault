3af3449511a0f917fc62c24500e0ce5e97a49ea1
Esteban <esteban.huenuman@hotmail.cl>
2022-05-14 22:13:58 +1200

    New theme to use Input field even disabled at full colour

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { NativeBaseProvider, Box } from 'native-base';
+import { NativeBaseProvider, Box, extendTheme } from 'native-base';
 
 import CreateAccount from './app/screens/CreateAccount';
 import PasswordsList from './app/screens/PasswordsList';
 import UserPasswordData from './app/screens/UserPasswordData';
 
-import { fetchBrandByName } from './api/Brandfetch';
-
 export default function App() {
-  // fetchBrandByName("facebook.com");
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
       {/* < CreateAccount /> */}
       {/* <PasswordsList /> */}
       <UserPasswordData action="new" />
@@ -18,3 +15,15 @@ export default function App() {
     </NativeBaseProvider>
   );
 }
+
+const theme = extendTheme({
+  components: {
+    Input: {
+      baseStyle: {
+        _disabled: {
+          opacity: 1
+        }
+      }
+    }
+  }
+});
