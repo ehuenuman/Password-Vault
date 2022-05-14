59e858c0453addad9f4a055dcb0a5c8fe8e0565d
Esteban <esteban.huenuman@hotmail.cl>
2022-05-14 22:17:33 +1200

    Form simplyfied and moved to one file

--- app/screens/UserPasswordData/index.js
+++ app/screens/UserPasswordData/index.js
@@ -1,45 +1,139 @@
 import React, { useState } from 'react';
 import PropTypes from "prop-types";
-
+import { Formik } from 'formik';
 import {
+  Avatar,
   Box,
-  Image,
+  Button,
+  Icon,
+  IconButton,
   ScrollView,
+  VStack
 } from 'native-base';
-import AppBar from './components/AppBar';
+import { FontAwesome } from '@expo/vector-icons';
 
-import Form from './components/Form';
+import AppBar from './components/AppBar';
+import InputField from './components/InputField';
+import SuggestBrands from './components/SuggestBrands';
+import PasswordInputField from './components/PasswordInputField';
+import ModalCategories from './components/ModalCategories';
 
 function UserPasswordData({ action }) {
 
-  const [formAction, setFormAction] = useState(action);
+  const [formMode, setFormMode] = useState(action);
+  const [showModal, setShowModal] = useState(false);
 
-  const changeFormAction = (action) => {
-    setFormAction(action);
+  const changeFormMode = (action) => {
+    setFormMode(action);
+  }
+
+  const formInitialValues = {
+    logo: "",
+    accountName: "",
+    website: "",
+    user: "",
+    password: "",
+    passwordStrength: 0,
+    category: ""
   }
 
   return (
     <Box safeAreaTop>
-      <AppBar action={formAction} actionChangeHandler={changeFormAction} />
+      <AppBar action={formMode} actionChangeHandler={changeFormMode} />
 
       <ScrollView>
-        <Box
-          px="15%"
-        >
-          <Image
-            source={{
-              uri: "https://wallpaperaccess.com/full/317501.jpg"
+        <Box px="15%">
+          <Formik
+            initialValues={formInitialValues}
+            onSubmit={values => {
+              console.log(values);
+              modeForm == "edit" ?
+                changeFormMode("view") :
+                console.log("New register");
             }}
-            alt="Brand Logo"
-            size="85px"
-            my="10"
-            alignSelf="center"
-          />
-          <Form viewMode={formAction == "view" ? true : false} modeForm={formAction} actionChangeHandler={changeFormAction} />
+          >
+            {({ handleChange, handleBlur, handleSubmit, values }) => (
+              <VStack maxWidth="300px">
+                <Avatar
+                  bg="primary.500"
+                  source={{
+                    uri: values.logo
+                  }}
+                  alt="Brand Logo"
+                  size="xl"
+                  my="10"
+                  alignSelf="center"
+                >
+                  {values.accountName.toUpperCase().slice(0, 2)}
+                </Avatar>
+                <InputField
+                  label="Account Name"
+                  placeHolder="How you identify this account?"
+                  viewMode={formMode == "view" ? true : false}
+                  value={values.accountName}
+                  onChangeText={handleChange("accountName")}
+                  onBlur={handleBlur("accountName")}
+                />
+                <SuggestBrands query={values.accountName} />
+                <InputField
+                  label="Website"
+                  placeholder="Where you go to do login?"
+                  viewMode={formMode == "view" ? true : false}
+                  value={values.website}
+                  onChangeText={handleChange("website")}
+                  onBlur={handleBlur("website")}
+                />
+                <InputField
+                  label="User"
+                  value={values.user}
+                  viewMode={formMode == "view" ? true : false}
+                  onChangeText={handleChange("user")}
+                  onBlur={handleBlur("user")}
+                  rightElement={(formMode == "view" ? true : false) &&
+                    <IconButton
+                      icon={<Icon as={FontAwesome} name="copy" />}
+                      borderRadius="full"
+                    />
+                  }
+                />
+                <PasswordInputField
+                  label="Password"
+                  value={values.password}
+                  viewMode={formMode == "view" ? true : false}
+                  hasPasswordChecker={true}
+                  onChangeText={handleChange("password")}
+                  onBlur={handleBlur("password")}
+                />
+                <InputField
+                  isDisabled={true}
+                  label="Category"
+                  value={values.category}
+                  viewMode={formMode == "view" ? true : false}
+                  // onChangeText={handleChange("categories")}
+                  // onBlur={handleBlur("categories")}
+                  leftElement={(formMode == "view" ? false : true) &&
+                    <Button
+                      variant="outline"
+                      onPress={() => setShowModal(true)}
+                    >
+                      {values.category ? "Change category" : "Select category"}
+                    </Button>
+                  }
+                  borderWidth="0"
+                />
+                <ModalCategories isOpen={showModal} setShowModal={setShowModal} />
+                {
+                  (!formMode == "view" ? false : true) &&
+                  <Button mt={10} onPress={handleSubmit}>
+                    SAVE
+                  </Button>
+                }
+              </VStack>
+            )}
+          </Formik>
         </Box>
       </ScrollView>
     </Box>
-
   )
 }
 
