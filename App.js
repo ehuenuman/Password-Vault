bcc8a64c10fdf645b01b155fa2c33f506b8e72bd
Esteban <esteban.huenuman@hotmail.cl>
2022-05-24 11:28:49 +1200

    Splashscreen till everything is ready

--- App.js
+++ App.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import { NativeBaseProvider, extendTheme } from 'native-base';
+import React, { useCallback, useEffect, useState } from 'react';
+import { NativeBaseProvider, extendTheme, View } from 'native-base';
+import * as SplashScreen from 'expo-splash-screen';
 
 import CreateAccount from './app/screens/CreateAccount';
 import PasswordsList from './app/screens/PasswordsList';
 import UserPasswordData from './app/screens/UserPasswordData';
+import { getAllEncryptedData } from './api/userPasswordData';
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [encryptedData, setEncryptedData] = useState();
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await getAllEncryptedData().then(
+          data => {
+            console.log("fetching data");
+            setEncryptedData(data);
+          },
+          () => console.log("Fail"));
+      } catch (error) {
+        console.warn(error)
+      } finally {
+        console.log("finally");
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <NativeBaseProvider theme={theme}>
-      {/* < CreateAccount /> */}
-      {/* <PasswordsList /> */}
-      <UserPasswordData action="new" />
-
+      <View onLayout={onLayoutRootView} flex={1}>
+        {/* < CreateAccount /> */}
+        <PasswordsList data={encryptedData} />
+        {/* <UserPasswordData action="new" /> */}
+      </View>
     </NativeBaseProvider>
   );
 }
