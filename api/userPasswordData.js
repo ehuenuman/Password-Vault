1b6debd86da4f419d812b1c2407b2d1c8680e623
Esteban <esteban.huenuman@hotmail.cl>
2022-05-24 11:32:20 +1200

    new: getAllEncryptedData

--- api/userPasswordData.js
+++ api/userPasswordData.js
@@ -1,21 +1,38 @@
-import { getFirestore, doc, updateDoc, arrayUnion, Timestamp } from "firebase/firestore";
+import { getFirestore, Timestamp, collection, addDoc, setDoc, getDocs, getDoc, doc } from "firebase/firestore";
 
 import { app } from "./firebaseConfig";
 
 const db = getFirestore(app);
 
-export default async function createUserPasswordData(data) {
+const USER_ID = "C9OB3RzOAt9tW4pOqC3r"
+
+export async function createUserPasswordData(data) {
   const values = {
     ...data,
     dataType: "password",
+    accountType: data.accountName,
     createTimestamp: Timestamp.now(),
     updateTimestamp: Timestamp.now()
   };
 
   console.log(values);
 
-  const userRef = doc(db, "Users", "yGtgKeZuQE1zg67vvb7P");
-  await updateDoc(userRef, {
-    passwords: arrayUnion(values)
+  const passwordCollectionRef = collection(db, "Users", USER_ID + "/passwords",);
+  const docRef = await addDoc(passwordCollectionRef, values);
+  console.log("Document written wirh ID: ", docRef.id);
+}
+
+export async function getAllEncryptedData() {
+  const colllectionRef = collection(db, "Users", USER_ID + "/passwords");
+  const querySnapshot = await getDocs(colllectionRef);
+  var passwords = [];
+
+  querySnapshot.forEach(doc => {
+    //console.log(doc.id, " => ", doc.data());
+    passwords.push({
+      ...doc.data(),
+      "id": doc.id
+    });
   });
+  return passwords
 }
