145ee25003111ac92a838a6e4917002132ad17db
Esteban <esteban.huenuman@hotmail.cl>
2022-05-25 18:29:35 +1200

    Update: The new register is creating using a own ID

--- api/userPasswordData.js
+++ api/userPasswordData.js
@@ -7,19 +7,21 @@ const db = getFirestore(app);
 const USER_ID = "C9OB3RzOAt9tW4pOqC3r"
 
 export async function createUserPasswordData(data) {
-  const values = {
-    ...data,
-    dataType: "password",
-    accountType: data.accountName,
-    createTimestamp: Timestamp.now(),
-    updateTimestamp: Timestamp.now()
-  };
+  // const values = {
+  //   ...data,
+  //   dataType: "password",
+  //   accountType: data.accountName,
+  //   createTimestamp: Timestamp.now(),
+  //   updateTimestamp: Timestamp.now()
+  // };
 
-  console.log(values);
+  //console.log(values);
 
-  const passwordCollectionRef = collection(db, "Users", USER_ID + "/passwords",);
-  const docRef = await addDoc(passwordCollectionRef, values);
-  console.log("Document written wirh ID: ", docRef.id);
+  // const passwordCollectionRef = collection(db, "Users", USER_ID + "/passwords");
+  const ref = doc(db, "Users", USER_ID + "/passwords/" + data.id);
+  // const docRef = await addDod(passwordCollectionRef, values, );
+  await setDoc(ref, data);
+  //console.log("Document written wirh ID: ", docRef.id);
 }
 
 export async function getAllEncryptedData() {
