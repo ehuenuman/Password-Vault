ee1885ec076bf0a969c939a6fd3a7c015b281724
Esteban <esteban.huenuman@hotmail.cl>
2022-06-13 19:36:22 +1200

    Auth Firebase actions The auth system is provided by Firebase. So all the functions previously defined in user.js were migrated to auth.js.

---
+++ api/auth.js
@@ -0,0 +1,138 @@
+import { signInAnonymously, createUserWithEmailAndPassword, signOut, updateProfile, signInWithEmailAndPassword } from "firebase/auth";
+import * as Crypto from 'expo-crypto';
+import * as SecureStore from 'expo-secure-store';
+
+import { auth } from "./firebaseConfig";
+import { createUserDatabase } from "./user";
+
+/**
+ * Asynchronously signs in using an email and password.
+ * 
+ * @remarks
+ * Fails with an error if the email address and password do not match.
+ * 
+ * @param {object} userData `Object` that contain the user/email and the password typed by the user.
+ * @return `Object` with the validity status and a message. If `isValid` is `true` then `message` is the user's ID that will be used as `userToken`.
+ */
+export async function signInUser(userData) {
+  var response = {
+    isValid: false,
+    message: "Failed login"
+  }
+  return signInWithEmailAndPassword(auth, userData.email, userData.masterPassword)
+    .then(async userCredential => {
+      return userCredential.user.getIdToken()
+        .then(async token => {
+          response.isValid = true;
+          response.message = token;
+
+          await SecureStore.setItemAsync("cypher", userData.masterPassword);
+
+          return response
+        })
+        .catch(error => {
+          console.warn(error);
+
+          return response;
+        });
+    })
+    .catch(error => {
+      // List of Auth errors: https://firebase.google.com/docs/auth/admin/errors
+      switch (error.code) {
+        case "auth/too-many-request":
+          response.message = "Account has been temporarily disabled due to many failed login attempts";
+          break;
+        case "auth/wrong-password":
+          response["message"] = "Wrong password";
+          break;
+        case "auth/user-not-found":
+          response.message = "Email does not have account";
+          break;
+        default:
+          response.message = "Failed login";
+          break;
+      }
+      return response
+    });
+}
+
+/**
+ * Create a new user into the Firebase Auth.
+ * 
+ * Check list fo errors: 
+ * https://firebase.google.com/docs/reference/js/v8/firebase.auth.Auth#createuserwithemailandpassword
+ * 
+ * @param {object} newUserData An `Object' with the data of the new user.
+ * @return The token of the newly registered user.
+ */
+export async function signUpUser(newUserData) {
+  // const hash = await sha512(newUserData.masterPassword)
+  //   .catch(error => console.error(error));
+  return createUserWithEmailAndPassword(auth, newUserData.email, newUserData.masterPassword)
+    .then(async userCredential => {
+      await updateProfile(userCredential.user, { displayName: newUserData.userName })
+        .catch(error => console.error(error));
+      await createUserDatabase(userCredential.user, newUserData);
+      return userCredential.user.getIdToken()
+        .then(userToken => userToken)
+        .catch(error => console.error(error));
+    })
+    .catch(error => console.error(error.code + " | " + error.message));
+}
+
+/**
+ * Delete the auth user instance. 
+ */
+export function signOutUser() {
+  signOut(auth)
+    .then(() => console.log("Sign out successful"))
+    .catch(error => console.error(error));
+}
+
+/**
+ * Initialise a instance of a anonymous user.
+ * 
+ * @returns `userToken`
+ */
+export async function anonymousUser() {
+  const userCredential = await signInAnonymously(auth)
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error("Error code: %0. %1", [errorCode, errorMessage]);
+    });
+  let userToken = await userCredential.user.getIdToken()
+    .catch(error => console.error(error));
+
+  return userToken
+}
+
+/**
+ * Verify the password matches with the hash. 
+ * 
+ * @param {string} password Characters that will be compared.
+ * @param {string} hash Hash string that will be compared.
+ * @returns Return a `bolean` indicating if the password is correct.
+ */
+async function isCorrectPassword(password, hash) {
+  const tempHash = await sha512(password);
+  return (tempHash === hash)
+}
+
+/**
+ * Calculate the hash of a string using **sha512 algorithm**.
+ * 
+ * @param {string} text String to hashing
+ * @returns sha512 digest
+ */
+async function sha512(text) {
+  var digest = "";
+  await Crypto.digestStringAsync(
+    Crypto.CryptoDigestAlgorithm.SHA512,
+    text
+  )
+    .then(hash => digest = hash)
+    .catch(err => console.warn(err));
+
+  return digest
+}
--- api/user.js
+++ api/user.js
@@ -1,101 +1,30 @@
-import { getFirestore, Timestamp, collection, addDoc, setDoc, getDocs, getDoc, doc, query, where } from "firebase/firestore";
-import * as Crypto from 'expo-crypto';
+import { collection, setDoc, getDocs, doc, query, where } from "firebase/firestore";
 
-import { app } from "./firebaseConfig";
-
-const db = getFirestore(app);
+import { firestore } from "./firebaseConfig";
 
 /**
- * Verify whether the email is available to be used by the new user.
+ * Verify if the email is available to be used by the new user.
  * 
- * @param {string} email 
+ * @param {string} email user's email.
  * @returns Availability of the email.
  */
 export async function isEmailAvailable(email) {
-  const q = query(collection(db, "users"), where("email", "==", email));
+  const q = query(collection(firestore, "users"), where("email", "==", email));
   const querySnapshoot = await getDocs(q);
 
   return (querySnapshoot.size === 0) ? true : false
 }
 
 /**
- * Check the user credentials.
- * `logInUser()` is used when the user has not done loggin in before.
+ * Create the user document into the Firestore that will be used to store the passwords' user.
  * 
- * @param {object} loginData `Object` that contain the user/email and the password typed by the user.
- * @returns `Object` with the validity status and a message. If `isValid` is `true` then `message` is the user's ID that will be used as `userToke`.
+ * @param {User} userInstance `User` object created after Signed Up a new user.
+ * @param {object} userData An `Object` with extra data from the register form.
  */
-export async function logInUser(loginData) {
-  var response = {
-    isValid: false,
-    message: ""
-  }
-  const q = query(collection(db, "users"), where("email", "==", loginData.email));
-  const querySnapshoot = await getDocs(q);
-  if (querySnapshoot.size === 0) {
-    response = { isValid: false, message: "User does not exist" }
-  } else {
-    await isCorrectPassword(loginData.masterPassword, querySnapshoot.docs[0].data().hash)
-      .then(isCorrect => {
-        response["isValid"] = isCorrect;
-        if (isCorrect)
-          response["message"] = querySnapshoot.docs[0].id
-        else
-          response["message"] = "Wrong password"
-      })
-      .catch(e => console.error(e));
-  }
-  return response
-}
-
-/**
- * Create a new user into the database.
- * 
- * @param {object} registerData An `Object` with the data of the new user.
- * @returns User ID to be used as userToken.
- */
-export async function signUpUser(registerData) {
-  var newUser = {
-    name: registerData.userName,
-    email: registerData.email,
-    dateJoined: Timestamp.now()
-  }
-  //TO DO: Use a better hashing method like bcrypt.
-  await sha512(registerData.masterPassword)
-    .then(hash => newUser = { ...newUser, hash: hash })
-    .catch(e => console.error(e));
-
-  const docRef = await addDoc(collection(db, "users"), newUser);
-
-  return docRef.id
-}
-
-/**
- * Verify the password matches with the hash. 
- * 
- * @param {string} password Characters that will be compared.
- * @param {string} hash Hash string that will be compared.
- * @returns Return a `bolean` indicating if the password is correct.
- */
-async function isCorrectPassword(password, hash) {
-  const tempHash = await sha512(password);
-  return (tempHash === hash)
-}
-
-/**
- * Calculate the hash of a string using **sha512 algorithm**.
- * 
- * @param {string} text String to hashing
- * @returns sha512 digest
- */
-async function sha512(text) {
-  var digest = "";
-  await Crypto.digestStringAsync(
-    Crypto.CryptoDigestAlgorithm.SHA512,
-    text
-  )
-    .then(hash => digest = hash)
-    .catch(err => console.warn(err));
-
-  return digest
+export async function createUserDatabase(userInstance, userData) {
+  await setDoc(doc(firestore, "users", userInstance.uid), {
+    email: userData.email,
+    passwordStrength: userData.passwordStrength
+  })
+    .catch(error => console.error(error));
 }
