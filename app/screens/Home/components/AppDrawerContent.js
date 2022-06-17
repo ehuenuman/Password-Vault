f359acb6688651a7438750176dc5310daf78715c
Esteban <esteban.huenuman@hotmail.cl>
2022-06-18 00:23:49 +1200

    Update: Drawer shows a user section at the top.

--- app/screens/Home/components/AppDrawerContent.js
+++ app/screens/Home/components/AppDrawerContent.js
@@ -1,24 +1,36 @@
 import React, { useContext } from 'react';
 import { DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer';
-import { Box, VStack } from 'native-base';
+import { Avatar, Box, Divider, HStack, Image, Text, VStack } from 'native-base';
 
 import { AuthContext } from '../../../data/AuthContext';
 
 function AppDrawerContent(props) {
 
-  const { signOut } = useContext(AuthContext);
+  const { signOut, loggedUser } = useContext(AuthContext);
 
   return (
     <DrawerContentScrollView {...props}>
-      <VStack space="3">
+      <VStack space="2">
+        <HStack space="3" mx="3" my="4">
+          <Image
+            alt="Secure Password Logo"
+            borderRadius="100"
+            resizeMode="contain"
+            source={require("../../../assets/icon_bg_color.png")}
+            size="sm"
+          />
+          <VStack space="1" justifyContent="center">
+            <Text bold fontSize="lg">{loggedUser().displayName}</Text>
+            <Text>{loggedUser().email}</Text>
+          </VStack>
+        </HStack>
+        <Divider bgColor="tertiary.700" />
 
         <DrawerItemList {...props} />
-        <DrawerItem label="Sign Out"
-          onPress={() => signOut()}
-        />
+        <DrawerItem label="Sign Out" onPress={() => signOut()} />
       </VStack>
 
-    </DrawerContentScrollView>
+    </DrawerContentScrollView >
   );
 }
 
