05df13b8b2d008283ba88096043462dc73c8fc3b
Esteban <esteban.huenuman@hotmail.cl>
2022-06-30 11:11:59 +1200

    Formatting code

--- app/screens/Login/index.js
+++ app/screens/Login/index.js
@@ -59,7 +59,12 @@ function Login({ route, navigation }) {
           }}
         >
           <VStack space="10">
-            <Image source={require("../../assets/logo_bg_transparent.png")} alt="Security Pass - All in one place" size="xl" resizeMode="contain" alignSelf="center" />
+            <Image
+              source={require("../../assets/logo_bg_transparent.png")}
+              alt="Security Pass - All in one place"
+              size="xl"
+              resizeMode="contain"
+              alignSelf="center" />
             <VStack>
               <VStack space="5">
                 <FormControl isInvalid={isFailLogin || (touched.email && errors.email)}>
